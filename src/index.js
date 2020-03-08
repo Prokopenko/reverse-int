d7145fb07612fb151a743b605731bb5793a23fa2
Prokopenko <alex27prok@gmail.com>
2020-03-09 00:42:01 +0300

    fix: Add function, which create reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  var i = Math.abs(n);
+    var result = 0;
+    while(i>0)
+    {
+        result = result*10 + i%10;
+        i = (i - i%10)/10;
+    }
   
+    return result;
 }
